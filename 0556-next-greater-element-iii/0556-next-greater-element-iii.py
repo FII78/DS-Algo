67744f3f140eee9db6bf2102d3b1475a7d0ac7a3
Fitsum Sileshi <fitsesile@gmail.com>
2022-12-27 09:45:48 +0300

    Time: 37 ms (76.76%), Space: 13.9 MB (78.70%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def nextGreaterElement(self, n: int) -> int:
+        nums = list(map(int,str(n)))
+        
+        i = len(nums)-1
+        
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+            
+        if i == 0:
+            nums.reverse()
+            opt = int("".join(list(map(str, nums))))
+            if opt < 2 ** 31 - 1 and opt > n:
+                return opt
+            else:
+                return -1
+        
+        j = len(nums)-1
+        
+        while j > 0 and nums[i-1] >= nums[j]:
+            j -= 1
+        
+        nums[i-1], nums[j] = nums[j], nums[i-1]
+        nums[i:] = nums[-1:i-1:-1]
+        opt =  int("".join(list(map(str, nums))))
+        if opt <= (2 ** 31 - 1) and opt > n:
+            return opt
+        else:
+            return -1
+        
