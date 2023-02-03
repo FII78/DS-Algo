b6f5985aec55eb0f8d916af894b9b696dfd6158d
Fitsum Sileshi <fitsesile@gmail.com>
2023-02-03 09:11:57 +0300

    Time: 172 ms (38.75%), Space: 14.6 MB (14.39%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,19 +1,25 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s)-1
-        opt = list(s)
-        need = 0
-        while l < r:
-            if opt[l] != opt[r]:
-                s1 = opt[:l] + opt[l+1:]
-                s2 = opt[:r] + opt[r+1:]
-                return s1 == s1[::-1] or s2==s2[::-1]
-         
-            else:
+        def delete(s, l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                
                 l += 1
                 r -= 1
+            return True
+               
+        cnt = 0
+        
+        l,r = 0, len(s)-1
+        
+        while l <= r:
+            if s[l] != s[r]:
+                return delete(s, l+1, r) or delete(s, l, r-1)  
+            l += 1
+            r -= 1
             
         return True
+                
             
-        
         
